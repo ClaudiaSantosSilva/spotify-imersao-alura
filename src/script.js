6dde24df994aef94fa1bbc6ed0767b47e285a752
ClaudiaSantosSilva <claudia.tss@outlook.com>
2024-01-26 17:35:50 +0000

    JS incluído

--- src/script.js
+++ src/script.js
@@ -4,15 +4,35 @@ const resultPlaylist = document.getElementById ("playlist");
 
 /*consumir API*/
 function requestAPI(searchTerm){
-    const url="http://localhost:3000/artists" /*qdo backend entregar a API, é só colocar o endereço aqui*/
-    fetch()
+    const url= `http://localhost:3000/artists?name_like=${searchTerm}` /*qdo backend entregar a API, é só colocar o endereço aqui*/
+    fetch(url)
+    .then((response)=>response.json())
+    .then((result)=>displayResults(result))
+}
+
+function displayResults(result){
+    resultPlaylist.classList.add("hidden");
+    const artistName = document.getElementById("artist-name");
+    const artistImage = document.getElementById("artist-img");
+
+    result.forEach(element => { /*isso é uma iteração*/
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;    
+    });
+
+    resultsArtist.classList.remove("hidden");
+
 }
 
 document.addEventListener ("input", function(){
-    const searchTerm = searchInput.ariaValueMax.toLowerCase();
+    const searchTerm = searchInput.value.toLowerCase();
+    console.log(searchTerm)
     if (searchTerm === ""){
-        resultPlaylist.classList.add("hidden"); /*incluir playlist*/
-        resultsArtist.classList.remove("hidden");/*retirar artistas*/
+        resultPlaylist.classList.remove("hidden"); /*incluir playlist*/
+        resultsArtist.classList.add("hidden");/*retirar artistas*/
         return; /*return pára a execução do código*/ 
        }
+       resultPlaylist.classList.add("hidden"); 
+       resultsArtist.classList.remove("hidden");  
+       requestAPI(searchTerm);
 })
